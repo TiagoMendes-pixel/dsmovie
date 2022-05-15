a0c7cebcf2fbf5fbdd86907a8f5727bf984015e2
TiagoMendes-pixel <62443975+TiagoMendes-pixel@users.noreply.github.com>
2022-05-15 12:47:59 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,41 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
+import { useState, useEffect} from "react";
+import { MoviePage } from "types/movie";
+import { setSourceMapRange } from "typescript";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
+useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+    .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPage(data);
+    });
+
+}, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+        
+    }
 
     return (
         <>
@@ -42,3 +76,7 @@ function Listing() {
 }
 
 export default Listing;
+
+function setPage(data: MoviePage) {
+    throw new Error("Function not implemented.");
+}
